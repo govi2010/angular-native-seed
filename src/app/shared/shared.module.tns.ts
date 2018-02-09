1099c18409bcc4fdda170c6a0f7f2e42bc13e856
Govinda Alwani <govi2010@gmail.com>
2018-02-09 11:55:22 +0530

    Vishal | shared module tns fle

--- src/app/shared/shared.module.tns.ts
+++ src/app/shared/shared.module.tns.ts
@@ -1,14 +1,51 @@
-import { NO_ERRORS_SCHEMA, NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NativeScriptCommonModule } from "nativescript-angular/common";
+import { NativeScriptUISideDrawerModule } from "nativescript-pro-ui/sidedrawer/angular";
+import { NativeScriptUIGaugesModule } from "nativescript-pro-ui/gauges/angular";
 
-import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
-import { NativeScriptFormsModule } from 'nativescript-angular/forms';
+import { MyDrawerItemComponent } from "./my-drawer-item/my-drawer-item.component";
+import { MyDrawerComponent } from "./my-drawer/my-drawer.component";
+import { MyButonComponent } from "./my-button/my-botton.component";
+import { MyLogoutComponent } from "./logout-button/logout-botton.component";
+import { PieChartComponent } from "~/shared/pie-chart/pie-chart.component";
+import { DropDownModule } from "nativescript-drop-down/angular";
+import { TNSCheckBoxModule } from 'nativescript-checkbox/angular';
+import { MyChipsComponent } from "~/shared/my-chips/my-chips.component";
+import { TNSFontIconModule } from "nativescript-ngx-fonticon";
 
 @NgModule({
-    declarations: [],
-    exports: [
-        NativeScriptModule,
-        NativeScriptFormsModule
+    imports: [
+        NativeScriptCommonModule,
+        NativeScriptUISideDrawerModule,
+        NativeScriptUIGaugesModule,
+        DropDownModule,
+        TNSCheckBoxModule,
+        TNSFontIconModule.forRoot({
+            'fa': './assets/font-awesome.min.css'
+        })
     ],
-    schemas: [ NO_ERRORS_SCHEMA ]
+    declarations: [
+        MyDrawerComponent,
+        MyDrawerItemComponent,
+        MyButonComponent,
+        MyLogoutComponent,
+        PieChartComponent,
+        MyChipsComponent
+    ],
+    exports: [
+        MyDrawerComponent,
+        MyButonComponent,
+        MyLogoutComponent,
+        MyChipsComponent,
+        PieChartComponent,
+        NativeScriptUISideDrawerModule,
+        NativeScriptUIGaugesModule,
+        TNSCheckBoxModule,
+        DropDownModule,
+        TNSFontIconModule
+    ],
+    schemas: [
+        NO_ERRORS_SCHEMA
+    ]
 })
 export class SharedModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,51 +1,39 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
-import { NativeScriptCommonModule } from "nativescript-angular/common";
-import { NativeScriptUISideDrawerModule } from "nativescript-pro-ui/sidedrawer/angular";
-import { NativeScriptUIGaugesModule } from "nativescript-pro-ui/gauges/angular";
 
 import { MyDrawerItemComponent } from "./my-drawer-item/my-drawer-item.component";
 import { MyDrawerComponent } from "./my-drawer/my-drawer.component";
 import { MyButonComponent } from "./my-button/my-botton.component";
 import { MyLogoutComponent } from "./logout-button/logout-botton.component";
-import { PieChartComponent } from "~/shared/pie-chart/pie-chart.component";
-import { DropDownModule } from "nativescript-drop-down/angular";
-import { TNSCheckBoxModule } from 'nativescript-checkbox/angular';
-import { MyChipsComponent } from "~/shared/my-chips/my-chips.component";
-import { TNSFontIconModule } from "nativescript-ngx-fonticon";
+import { PieChartComponent } from "./pie-chart/pie-chart.component";
+import { MyChipsComponent } from "./my-chips/my-chips.component";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
-  imports: [
-    NativeScriptCommonModule,
-    NativeScriptUISideDrawerModule,
-    NativeScriptUIGaugesModule,
-    DropDownModule,
-    TNSCheckBoxModule,
-    TNSFontIconModule.forRoot({
-      'fa': './assets/font-awesome.min.css'
-    })
-  ],
-  declarations: [
-    MyDrawerComponent,
-    MyDrawerItemComponent,
-    MyButonComponent,
-    MyLogoutComponent,
-    PieChartComponent,
-    MyChipsComponent
-  ],
-  exports: [
-    MyDrawerComponent,
-    MyButonComponent,
-    MyLogoutComponent,
-    MyChipsComponent,
-    PieChartComponent,
-    NativeScriptUISideDrawerModule,
-    NativeScriptUIGaugesModule,
-    TNSCheckBoxModule,
-    DropDownModule,
-    TNSFontIconModule
-  ],
-  schemas: [
-    NO_ERRORS_SCHEMA
-  ]
+    imports: [
+        CommonModule,
+        RouterModule,
+        FormsModule,
+        ReactiveFormsModule,
+    ],
+    declarations: [
+        MyDrawerComponent,
+        MyDrawerItemComponent,
+        MyButonComponent,
+        MyLogoutComponent,
+        PieChartComponent,
+        MyChipsComponent
+    ],
+    exports: [
+        MyDrawerComponent,
+        MyButonComponent,
+        MyLogoutComponent,
+        MyChipsComponent,
+        PieChartComponent,
+    ],
+    schemas: [
+        NO_ERRORS_SCHEMA
+    ]
 })
 export class SharedModule { }

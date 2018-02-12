af2a3fd0e9176bfd77a96c2ff25e401bd1b2c80d
Govinda Alwani <govi2010@gmail.com>
2018-02-12 17:41:09 +0530

    Vishal | sales component

--- src/app/salesInvoice/salesInvoice.component.ts
+++ src/app/salesInvoice/salesInvoice.component.ts
@@ -3,7 +3,7 @@ import { OnInit, Component } from "@angular/core";
 @Component({
     selector: 'ns-sales-invoice',
     moduleId: module.id,
-    templateUrl: `./salesInvoice.component.html`,
+    templateUrl: './salesInvoice.component.html',
     styleUrls: ['./salesInvoice.component.scss']
 })
 export class SalesInvoiceComponent implements OnInit {

3e2ef7a3f449b741ed56c3b766e805f4a5d56dcb
Govinda Alwani <govi2010@gmail.com>
2018-02-13 14:36:07 +0530

    Vishal | currencies list

--- src/app/settings/components/currencies/currencies.component.ts
+++ src/app/settings/components/currencies/currencies.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
+import { RouterService } from '../../../services/router.service';
+import { Observable } from 'rxjs/Observable';
+import { ReplaySubject } from 'rxjs/ReplaySubject';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../../store';
 
 @Component({
     selector: 'ns-currencies',
@@ -6,13 +11,19 @@ import { Component, OnInit, ViewChild } from '@angular/core';
     templateUrl: './currencies.component.html'
 })
 
-export class CurrenciesComponent implements OnInit {
-
-    constructor() {
-
+export class CurrenciesComponent implements OnInit, OnDestroy {
+    public currenciesStream$: Observable<string[]>;
+    private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
+    constructor(private store: Store<AppState>, private routerExtensions: RouterService) {
+        this.currenciesStream$ = this.store.select(state => state.general.currencies).takeUntil(this.destroyed$);
     }
 
     public ngOnInit() {
         //
+    }
+
+    public ngOnDestroy() {
+        this.destroyed$.next(true);
+        this.destroyed$.complete();
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,18 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/take';
+import 'rxjs/add/operator/takeUntil';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/combineLatest';
+
 if (environment.production) {
-  enableProdMode();
+    enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
